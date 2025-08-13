603db8bbd6142c565909469ab56edbb2e9e52855
Santanu Mondal <santanumondal4168@gmail.com>
2025-08-13 14:01:33 +0530

    React router added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Pokedex from './Components/Pokedex/Pokedex'
+import CustomRoutes from './routes/CustomRoutes'
+import { Link } from 'react-router-dom'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <Pokedex/>
-    </>
+    <div className="outer-pokedex">
+     <Link to="/"><h1 id="pokedex-heading">Pokedex</h1></Link>
+      <CustomRoutes/>
+    </div>
   )
 }
 
--- src/Components/Pokemon/Pokemon.jsx
+++ src/Components/Pokemon/Pokemon.jsx
@@ -1,9 +1,13 @@
 import "./Pokemon.css"
-export default function Pokemon({name,image}){
+import { Link } from "react-router-dom"
+export default function Pokemon({name,image,id}){
     return(
+
         <div className="pokemon">
+        <Link to={`/pokemon/${id}`}>
         <div>{name}</div>
         <div>{image==null? "No image found":<img className="pokemon-image" src={image}/>}</div>
+        </Link>
         </div>
     )
 }
